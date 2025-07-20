3b9711f0ac561672456c60fcc5b152e502b5f9e9
Mayank443 <mayank04042003@gmail.com>
2025-07-20 15:07:48 +0530

    1st commit -setup

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:fitnesss_app/screens/OnboardingScreen/onboarding_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      color: Colors.black,
+      debugShowCheckedModeBanner: false,
+      home: const OnBoardingScreen(),
+    );
+  }
+}
